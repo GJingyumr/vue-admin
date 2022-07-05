f8a36ca12c83c05b906184402200a25ee4c9a296
GuoJYmr <2676722984@qq.com>
2022-07-05 10:52:07 +0800

    feat(创建并基础配置 下载了element-plus和axios 配置跨域): 创建并基础配置 下载了element-plus和axios 配置跨域

---
+++ src/views/login/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>123456</div>
+</template>
+
+<script setup></script>
+<style scoped lang="scss"></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    port: 9999,
+    host: 'localhost',
+    https: false,
+    open: true,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  },
+  lintOnSave: true
+}
